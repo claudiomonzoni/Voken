3417ee6ac3e905ee9951e4334c7140c4a546e23a
claudiomonzoni <claudiomonzoni@hotmail.com>
2022-12-19 13:16:13 -0600

    avanzando

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+// submemu
+const sport = document.querySelector(".sport");
+const submenu = document.querySelector("#submenu");
 
 //only phones
+
 var x = window.matchMedia("(max-width: 479px)");
 esCell(x); // Call listener function at run time
 x.addListener(esCell); // Attach listener function on state changes
@@ -14,45 +18,36 @@ function esCell(x) {
     // const anchor = document.querySelector(".anchor");
     const abierto = false;
 
-    function abrirMenuFull () {
+    function abrirMenuFull() {
       this.abierto = !this.abierto;
-      console.log(abierto)
       mainNav.classList.remove("menuFullAbierto");
       mainNav.classList.remove("desanimaMenu");
       menuIco.classList.toggle("is-active");
-
+      
       this.abierto
-        ? mainNav.classList.toggle("menuFullAbierto")
-        : mainNav.classList.toggle("desanimaMenu");
-    };
-
+      ? mainNav.classList.toggle("menuFullAbierto")
+      : mainNav.classList.toggle("desanimaMenu");
+    }
+    
     menuIco.addEventListener("click", abrirMenuFull);
-    //anchor.addEventListener("click", abrirMenuFull);
+  
   } else {
     return;
   }
 }
 
-// submemu
-const sport = document.querySelector(".sport");
-const submenu = document.querySelector("#submenu");
-
-sport.addEventListener("mouseenter", function(e)
-{ 
-
- submenu.classList.add("visibleMenu")
-})
-window.addEventListener("click", function(e)
-{ 
- submenu.classList.remove("visibleMenu")
-})
+sport.addEventListener("mouseenter", function (e) {
+  submenu.classList.add("visibleMenu");
+});
+window.addEventListener("click", function (e) {
+  submenu.classList.remove("visibleMenu");
+});
 
 // importo gsap
-import gsap from 'gsap'
-gsap.from(".logo, ul>li",
-{
-  stagger:0.1,
-  y:100,
+import gsap from "gsap";
+gsap.from(".logo, ul>li", {
+  stagger: 0.1,
+  y: 100,
   opacity: 0,
-  duration: 1 //1 segundo
-})
+  duration: 1, //1 segundo
+});
