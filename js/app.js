52949f96b5b8b5e5e4ad59f489753bd57b3065d6
claudiomonzoni <claudiomonzoni@hotmail.com>
2022-12-28 11:33:35 -0600

    avance con drag para pc

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 // import Swiper from 'swiper';
-import {slideIndex} from './glide'
-slideIndex()
+import { slideIndex } from "./glide";
+slideIndex();
 // submemu
 const sport = document.querySelector(".sport");
 const submenu = document.querySelector("#submenu");
@@ -28,14 +28,13 @@ function esCell(x) {
       mainNav.classList.remove("menuFullAbierto");
       mainNav.classList.remove("desanimaMenu");
       menuIco.classList.toggle("is-active");
-      
+
       this.abierto
-      ? mainNav.classList.toggle("menuFullAbierto")
-      : mainNav.classList.toggle("desanimaMenu");
+        ? mainNav.classList.toggle("menuFullAbierto")
+        : mainNav.classList.toggle("desanimaMenu");
     }
-    
+
     menuIco.addEventListener("click", abrirMenuFull);
-  
   } else {
     return;
   }
@@ -48,9 +47,36 @@ window.addEventListener("click", function (e) {
   submenu.classList.remove("visibleMenu");
 });
 
-// importo gsap
+
+
+
+// logica para escrollear en pc
+const card_slide = document.querySelector(".slider");
+let arrastrable = false, prevPageX, prevScrollLeft;
+const arrastrame = (e) => {
+  arrastrable = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = card_slide.scrollLeft;
+};
+const arrastrar = (e) => {
+  if (!arrastrable) return;
+  e.preventDefault();
+  let posDiff = e.pageX - prevPageX
+  card_slide.scrollLeft = prevScrollLeft -posDiff
+};
+const yanosearrastra = () => {
+  arrastrable = false;
+};
+
+card_slide.addEventListener("mousedown", arrastrame);
+card_slide.addEventListener("mousemove", arrastrar);
+card_slide.addEventListener("mouseup", yanosearrastra);
+
+
+
+
 // gsap.registerPlugin(ScrollTrigger)
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".logo, nav>ul>li", {
   stagger: 0.1,
@@ -58,7 +84,6 @@ gsap.from(".logo, nav>ul>li", {
   opacity: 0,
   duration: 1, //1 segundo
 });
-
 
 gsap.from(".glide__slides>.news", {
   scrollTrigger: ".glide__slides>.news",
